0e7187073eec37c6c843ba61606d82e97d9a38ad
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-01-29 21:37:37 +0100

    Add the scaffolding
    Some of the structure comes from RamblingCookieMonster:
    http://ramblingcookiemonster.github.io/Building-A-PowerShell-Module/

---
+++ PSToggl/PSToggl.psm1
@@ -0,0 +1,16 @@
+# Credit goes to RamblingCookieMonster
+$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+Foreach($import in @($Public + $Private)) {
+    Try {
+        . $import.fullname
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+$formatFile = Join-Path -Path $PSScriptRoot -ChildPath 'PSJira.format.ps1xml'
+Write-Verbose "Updating format data with file '$formatFile'"
+Update-FormatData -AppendPath $formatFile -ErrorAction Continue
