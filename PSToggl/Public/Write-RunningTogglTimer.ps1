58c42892cd55f70ef99700b0f9a146bad48593ed
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-03-19 20:17:37 +0100

    Update RunningTogglTimer helper for prompt usage
    Will update the prompt text like posh git does.

--- PSToggl/Public/Write-RunningTogglTimer.ps1
+++ PSToggl/Public/Write-RunningTogglTimer.ps1
@@ -1,12 +1,21 @@
 function Write-RunningTogglTimer(){
+    param(
+        # Special case - format for prompt
+        [Switch]
+        $ForPrompt
+    )
     $Running = Get-TogglTimer -Current
     if ($Running.id -GT 0) {
-        Write-Host "TOGGL: " -NoNewline -ForegroundColor Yellow -BackgroundColor Black
-        if ($Running.description) {
-            Write-Host $Running.description -ForegroundColor Yellow -NoNewline -BackgroundColor Black
+        if (!$ForPrompt) {
+            Write-Host "TOGGL: " -NoNewline -ForegroundColor Yellow -BackgroundColor Black
+            if ($Running.description) {
+                Write-Host $Running.description -ForegroundColor Yellow -NoNewline -BackgroundColor Black
+            } else {
+                Write-Host "No Description" -ForegroundColor Red -NoNewline -BackgroundColor Black
+            }
+            Write-Host (" is running since " + $Running.Start + ")") -BackgroundColor Black
         } else {
-            Write-Host "No Description" -ForegroundColor Red -NoNewline -BackgroundColor Black
+            Write-Host (" [" + $Running.description + " since " + ([datetime]($a.at)).TimeOfDay.ToString()) -NoNewline -ForegroundColor Yellow
         }
-        Write-Host (" is running since " + $Running.Start + ")") -BackgroundColor Black
     }
 }
