8d232c833b9f3841bec20d1aa39f253df24e901f
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-03-19 21:39:18 +0100

    Write a little bit of help text for Get-TogglProject
    This also functions as brainstorming.

--- PSToggl/Public/Get-TogglProject.ps1
+++ PSToggl/Public/Get-TogglProject.ps1
@@ -1,28 +1,40 @@
 function Get-TogglProject {
-	<#
+    <#
     .Synopsis
-		Receives Toggl Projects based on their name
-    .DESCRIPTION 
+        Receives Toggl Projects
+    .DESCRIPTION
+        This cmdlet queries the Toggl API against projects. If no parameter is given, it returns all accessible projects.
+        You can search projects by its name (with wildcards) or by its id (without).
+        To filter for other properties (e.g. billable, at), pipe the result to Where-Object.
+        You can pipe any PSToggl object which belongs to a project to this cmdlet, like:
+        * Workspace: returns projects whose wid mathes the id of the piped object
+        * Client: returns projects whose cid matches the id of the piped object
+        * User (This is a special case, a user contains its projects as array. If not, will query for it and return)
     .EXAMPLE
+        Get-TogglProject -ProjectName "*awesome*"
+        Get-TogglProject | Where-Object billable
+        Get-TogglClient -name "Customer" | Get-TogglProject
     .INPUTS
+        System.Management.Automation.PSObject
+            PSToggl.Client
+            PSToggl.Workspace
+            PStoggl.User
     .OUTPUTS
+        System.Management.Automation.PSObject
+            PsToggl.Project
     #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$false)]
-		[string] $ProjectName = $null,
+        [string] $ProjectName = $null,
 
         [Parameter(Mandatory=$false)]
-		[string] $Workspace = $primaryWorkspace
+        [string] $Workspace = $primaryWorkspace
     )
     $projects = Invoke-TogglMethod -UrlSuffix "workspaces/$($Workspace)/projects"
 
-    If (!$projects) {
-        Throw [System.Exception]::new("No Toggl projects found")
-    } else {
-		if ($ProjectName) {
+        if ($ProjectName) {
             $projects = $projects | Where-Object name -Like $ProjectName
         }
         return $projects
-    }
 }
