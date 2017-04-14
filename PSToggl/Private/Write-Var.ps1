659e6b307e40966822a175545450fae488565295
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-04-14 15:59:31 +0200

    Add ToDos for pipeline usage, format Whitespaces

--- PSToggl/Private/Write-Var.ps1
+++ PSToggl/Private/Write-Var.ps1
@@ -1,9 +1,10 @@
+# TODO: Accept pipelpie inputs (e.g. for Invoke-TogglMethod.ps1)
 function Write-Var {
-	param(
-		# The name of the variable to verbose
-		[Parameter(Mandatory = $true)]
-		[String] $VarName
-	)
+    param(
+        # The name of the variable to verbose
+        [Parameter(Mandatory = $true)]
+        [String] $VarName
+    )
 
     Try {
         Write-Debug ((Get-Variable $VarName).Name.PadRight(12) + (Get-Variable $VarName).Value)
