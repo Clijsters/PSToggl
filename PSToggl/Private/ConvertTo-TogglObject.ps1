a60b8dc3c26196f2cb58a52a0e33d2ec5bf12f31
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-07-12 21:12:08 +0200

    Finish ConvertTo-TogglObject

--- PSToggl/Private/ConvertTo-TogglObject.ps1
+++ PSToggl/Private/ConvertTo-TogglObject.ps1
@@ -4,7 +4,7 @@ function ConvertTo-TogglObject {
         # A PSCustomObject to convert
         [Parameter( Mandatory = $true, Position = 0, ValueFromPipeline = $true )]
         [System.Management.Automation.PSCustomObject]
-        $input,
+        $InputObject, <#Alias?#>
 
         # The Object's name
         [Parameter(Mandatory = $true)]
@@ -30,36 +30,43 @@ function ConvertTo-TogglObject {
     )
 
 
-##
-## TODO TESTS
-##
-    $object = @{}
-    if ($item.GetType().Name -eq "HashTable") {
-        $input = New-Object -TypeName psobject -Property $item
-    } else {
-        $input = $item
-    }
-
-    foreach ($field in $objectConfig.Fields) {
-        $inputField = $input.PSObject.Members[$field.name].Value
-        if ($null -ne $inputField) {
-            $object[$field.name] = $inputField -as $field.type
-        } else {
-            if ($null -ne $field.default) {
-                $object[$field.name] = $field.default -as $field.type
-            } elseif ($field.required) {
-                throw "Property `"$($field.name)`" is required"
+    ##
+    ## TODO TESTS
+    ##
+    process {
+        foreach ($item in $InputObject) {
+            $object = @{}
+            if ($item.GetType().Name -eq "HashTable") {
+                $input = New-Object -TypeName psobject -Property $item
             }
-        }
-    }
+            else {
+                $input = $item
+            }
 
-    $result = New-Object -TypeName psobject -Property $object
-    $result.PSObject.TypeNames.Insert(0, "PSToggl.$ObjectName")
-    foreach ($validator in $objectConfig.Validators) {
-        if (-not $validator.callback.invoke($result)) {
-            Write-Debug ($validator.name + " returned false. Throwing ArgumentException with message: " + $validator.message)
-            Throw [System.ArgumentException]::new("Error validating fields: " + $validator.message)
+            foreach ($field in $objectConfig.Fields) {
+                $inputField = $input.PSObject.Members[$field.name].Value
+                if ($null -ne $inputField) {
+                    $object[$field.name] = $inputField -as $field.type
+                }
+                else {
+                    if ($null -ne $field.default) {
+                        $object[$field.name] = $field.default -as $field.type
+                    }
+                    elseif ($field.required) {
+                        throw "Property `"$($field.name)`" is required"
+                    }
+                }
+            }
+
+            $result = New-Object -TypeName psobject -Property $object
+            $result.PSObject.TypeNames.Insert(0, "PSToggl.$ObjectName")
+            foreach ($validator in $objectConfig.Validators) {
+                if (-not $validator.callback.invoke($result)) {
+                    Write-Debug ($validator.name + " returned false. Throwing ArgumentException with message: " + $validator.message)
+                    Throw [System.ArgumentException]::new("Error validating fields: " + $validator.message)
+                }
+            }
+            Write-Output $result
         }
     }
-    Write-Output $result
 }
