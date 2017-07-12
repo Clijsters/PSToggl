c0b774fcad575d56a5df097ad59c51aac183c78d
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-07-12 21:55:11 +0200

    Finish Start-TogglEntry Tests

--- Tests/PSToggl/Public/Start-TogglEntry.Tests.ps1
+++ Tests/PSToggl/Public/Start-TogglEntry.Tests.ps1
@@ -5,16 +5,33 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path)
 
 InModuleScope PSToggl {
     Describe "Start-TogglEntry" {
-        It "Calls Invoke-TogglMethod" {
-            Mock Invoke-TogglMethod {}
+        Mock Invoke-TogglMethod {}
+        Mock Get-TogglProject {
+            return @(
+                @{
+                    name = "test";
+                    id = 123
+                },
+                @{
+                    name = "test2";
+                    id = 456
+                }
+            )
+        }
+
+        It "Calls Invoke-TogglMethod with the correct url" {
             Start-TogglEntry
-            Assert-MockCalled -CommandName "Invoke-TogglMethod" -ParameterFilter {}
+            Assert-MockCalled -CommandName "Invoke-TogglMethod" -ParameterFilter {$UrlSuffix -eq "time_entries/start"}
         }
 
         It "Uses Get-TogglProject if a Project Name is given" {
-            Mock Get-TogglProject {}
             Start-TogglEntry -Description "Test" -ProjectName "Test Project"
             Assert-MockCalled -CommandName "Get-TogglProject" -ParameterFilter {$Name -eq "Test Project"}
+        }
+
+        It "Uses the first matching projects id to start an Entry on a given project" {
+            Start-TogglEntry -Description "Test" -ProjectName "Test Project"
+            Assert-MockCalled -CommandName "Invoke-TogglMethod" -ParameterFilter {$InputObject.time_entry.pid -eq 123}
         }
 
         It "Creates new Tags if necessary" {
