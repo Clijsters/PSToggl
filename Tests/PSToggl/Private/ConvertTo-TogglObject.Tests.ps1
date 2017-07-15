f0f4132be1166fedc5c2844914d598c458e09fb8
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-07-15 19:08:30 +0200

    Finish tests for ConvertTo-TogglObject

--- Tests/PSToggl/Private/ConvertTo-TogglObject.Tests.ps1
+++ Tests/PSToggl/Private/ConvertTo-TogglObject.Tests.ps1
@@ -5,16 +5,51 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path)
 
 InModuleScope PSToggl {
     Describe "ConvertTo-TogglObject" {
-        It "Creates a Client Object with all necessary attributes" {
+        $objectConfig = @{
+            TypeName = "PSToggl.Project";
+            DocUrl   = "";
+            Fields   = @(
+                @{ name = "id"; required = $false; default = $null; type = [int]; },
+                @{ name = "name"; required = $true; default = $null; type = [string]; },
+                @{ name = "wid"; required = $true; default = $null; type = [int]; },
+                @{ name = "cid"; required = $false; default = $null; type = [int]; },
+                @{ name = "active"; required = $true; default = $true; type = [bool]; },
+                @{ name = "is_private"; required = $true; default = $true; type = [bool]; },
+                @{ name = "at"; required = $true; default = $null; type = [datetime]; },
+                @{ name = "color"; required = $true; default = $null; type = [int]; }
+            );
+        };
 
+        $sampleInput = @{
+            name  = "asdf";
+            wid   = 123;
+            cid   = 123;
+            at    = [datetime]::Now;
+            color = 2;
         }
 
-        It "Creates a Task Object with all necessary attributes" {
+        $out = $sampleInput | ConvertTo-TogglObject -ObjectConfig $objectConfig
 
+        It "Sets TypeName to '$($objectConfig.TypeName)'" {
+            $out.PSObject.TypeNames[0] | Should Be $objectConfig.TypeName
         }
 
-        It "Creates an Entry Object with all necessary attributes" {
+        foreach ($k in $sampleInput.Keys) {
+            It "Sets $($k.PadRight(8)) to $($sampleInput.Item($k))" {
+                $out.PSObject.Members[$k].Value | Should Be $sampleInput.Item($k)
+            }
+        }
 
+        foreach ($field in ($objectConfig.Fields | Where-Object {-not ($_.name -in $sampleInput.Keys)})) {
+            It "sets not provided field $($field.name) to default value '$($field.default)'" {
+                $out.PSObject.Members[$field.name].Value | Should Be $field.default
+            }
+        }
+
+        It "Throws an error if a mandatory fields are missing" {
+            $t = $sampleInput
+            $t.name = $null
+            {ConvertTo-TogglObject -InputObject $t -ObjectConfig $objectConfig} | Should Throw
         }
     }
 }
