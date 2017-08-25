c048747a52a299bf4bb8c717fef3ed3e20ac5737
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-08-25 10:28:25 +0200

    Playing with CodeCov CI

--- AppVeyor/Test.ps1
+++ AppVeyor/Test.ps1
@@ -1,17 +1,21 @@
 Write-Host "Current working directory: $pwd"
-$testResultsFile = '.\TestsResults.xml'
+#$testResultsFile = '.\TestsResults.xml'
 $cc = Get-ChildItem ..\PSToggl -Recurse -File -Include *.ps1 -Exclude *.Tests.ps1
-$result = Invoke-Pester -Script ..\ -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru -CodeCoverage $cc
+#$result = Invoke-Pester -Script ..\ -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru -CodeCoverage $cc
+
 
 if ($env:APPVEYOR_JOB_ID) {
-    Write-Host 'Uploading results'
-    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
-    Write-Host "Deleting Result file"
-    Remove-Item $testResultsFile -Force -ErrorAction SilentlyContinue
+    Invoke-AppVeyorTestScriptTask -CodeCoverage -CodeCovIo
+    #Write-Host 'Uploading results'
+    #(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+    #Write-Host "Deleting Result file"
+    #Remove-Item $testResultsFile -Force -ErrorAction SilentlyContinue
+} else {
+    Invoke-Pester -Script ..\ -CodeCoverage $cc
 }
 
-if (($result.FailedCount -gt 0) -or ($result.PassedCount -eq 0)) {
+<#if (($result.FailedCount -gt 0) -or ($result.PassedCount -eq 0)) {
     throw "$($result.FailedCount) tests failed."
 } else {
     Write-Host 'All tests passed' -ForegroundColor Green
-}
+}#>
