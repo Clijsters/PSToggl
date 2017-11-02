14607563557de4f5c32f554446249e50146eaffa
Dominique Clijsters <dominique.clijsters@gmail.com>
2017-11-02 16:18:27 +0100

    InputObject Testing //FIXME: Needs Mocked Cmdlets

--- Tests/PSToggl/Public/Get-TogglClient.Tests.ps1
+++ Tests/PSToggl/Public/Get-TogglClient.Tests.ps1
@@ -6,6 +6,31 @@ $sut = Split-Path -Leaf $MyInvocation.MyCommand.Path
 InModuleScope PSToggl {
     Describe "Get-TogglClient" {
         $exampleObject = @{
+            id    = 1;
+            name  = "Test Client";
+            wid   = 123;
+            at    = [datetime]::Now;
+            notes = "Some note";
+        }
+
+        $exampleProject = @{
+            name  = "asdf";
+            id = 123;
+            wid   = 123;
+            cid   = 123;
+            at    = [datetime]::Now;
+            color = 2;
+        }
+
+        $multipleObjects = New-Object -TypeName System.Collections.ArrayList
+        0..9 | ForEach-Object {
+            $obj = $exampleObject.psobject.Copy()
+            $obj.id = $obj.id + $_
+            $multipleObjects.Add($obj)
+        }
+
+        Mock Get-TogglProject {
+            return $exampleProject
         }
 
         Mock Invoke-TogglMethod {
@@ -15,12 +40,15 @@ InModuleScope PSToggl {
                 [ValidateSet("GET", "POST", "PUT", "DELETE")]
                 [String] $Method
             )
-            return $exampleObject
+            if ($UrlSuffix -like "*projects*") {
+                return $exampleProject
+            } else {
+                return $exampleObject
+            }
         }
 
-        Mock ConvertTo-TogglClient {
-            #
-        }
+        Mock ConvertTo-TogglClient {}
+        Mock Write-Verbose {}
 
         It "Calls Invoke-TogglMethod" {
             {Get-TogglClient} | Should Not Throw
@@ -35,5 +63,21 @@ InModuleScope PSToggl {
         It "Returns the entries converted with ConvertTo-TogglClient" {
             (Get-TogglClient).description | Should Be $exampleObject.description
         }
+
+        It "Takes `$InputObject of type `"PSToggl.Entry`" and properly filters for related clients" {
+            @{
+                description = "Test entry";
+                wid         = 123;
+                pid         = 123;
+                tid         = 123;
+                start       = [datetime]::Now;
+                stop        = [datetime]::Now;
+                duration    = 0;
+                at          = [datetime]::Now;
+            } | ConvertTo-TogglEntry | Get-TogglClient
+        }
+
+        It "Takes `$InputObject of type `"PSToggl.Project`" and properly filters for related clients" {}
+
     }
 }
